62673612df4482b559e359f0e1961461d37f2e3e
sayem <mahadisayem2001@gmail.com>
2024-04-16 13:54:01 -0400

    Displaying critic score

--- video-games/src/components/GameCard.tsx
+++ video-games/src/components/GameCard.tsx
@@ -4,12 +4,14 @@ import {
   Card,
   CardBody,
   CardBodyProps,
+  HStack,
   Heading,
   Img,
   popperCSSVars,
 } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface props {
   game: Game;
@@ -21,9 +23,12 @@ const GameCard = ({ game }: props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
